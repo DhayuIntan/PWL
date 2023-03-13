6935a2a7be58c217e49245bdcd597649324bdc62
Dhayu Intan Nareswari <intaandh@gmail.com>
2023-03-13 18:37:10 +0700

    tes-modif

--- resources/views/artikel/index.blade.php
+++ resources/views/artikel/index.blade.php
@@ -37,7 +37,7 @@
           </div>
         </div>
         <div class="card-body">
-            <table id="example2" class="table table-bordered table-hover">
+            <table id="tabelartikel" class="table table-bordered table-hover">
                 <thead>
                 <tr>
                   <th>ID Artikel</th>
